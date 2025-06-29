a9d83fda145c024a776bd211607e15d0de27a645
ayoub chaib <watchpro2@gmail.com>
2025-06-29 20:03:18 +0100

    feat: initialize Spring Boot project with basic dependencies

--- backend/src/main/java/org/example/weatherstreams/digital_banking/DigitalBankingApplication.java
+++ backend/src/main/java/org/example/weatherstreams/digital_banking/DigitalBankingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DigitalBankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DigitalBankingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalBankingApplication.class, args);
+    }
 
 }
--- backend/src/test/java/org/example/weatherstreams/digital_banking/DigitalBankingApplicationTests.java
+++ backend/src/test/java/org/example/weatherstreams/digital_banking/DigitalBankingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DigitalBankingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
